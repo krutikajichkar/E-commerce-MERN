c711fa78e6aca546dc700ecebd8975b1b8b5479f
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2024-03-06 13:42:18 +0530

    products added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express'
-
+import { products } from './data/products.js';
 const app = express();
 
 const PORT = 5000;
 
 app.get('/',(req,res) => {
     res.send("Hello World!!");
+})
+
+app.get('/api/products' , (req,res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id' , (req,res) => {
+    const product = products.find((p) => p.id === Number(req.params.id) );
+    res.json(product);
 })
 
 app.listen(PORT , () => {
