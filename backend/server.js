739101f4384749532d81bc61974a5c61d98b8908
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2024-03-06 21:44:26 +0530

    connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,26 @@
-import express from 'express'
-import { products } from './data/products.js';
+import express from "express";
+import { products } from "./data/products.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+connectDB();
 const app = express();
 
-const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
 
-app.get('/',(req,res) => {
-    res.send("Hello World!!");
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products' , (req,res) => {
-    res.json(products)
-})
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === Number(req.params.id));
+  res.json(product);
+});
 
-app.get('/api/products/:id' , (req,res) => {
-    const product = products.find((p) => p.id === Number(req.params.id) );
-    res.json(product);
-})
-
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
