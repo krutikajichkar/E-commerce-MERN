8e0ebec1e38e11b5f9ee5b2aa94d8636fa3f73ab
Krutika Jichkar <93243210+krutikajichkar@users.noreply.github.com>
2024-03-06 10:56:28 +0530

    added packages

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontSize: {
+        'h1': '48px',
+        'h2': '36px',
+        'h3': '30px',
+        'h4': '24px',
+        'h5': '20px',
+        'h6': '16px',
+      },
+    },
+  },
+  plugins: [],
+}
+
